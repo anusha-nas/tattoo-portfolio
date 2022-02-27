2415757b787b7880db2ea46c614b4c8887a54239
anusha-nas <49104205+anusha-nas@users.noreply.github.com>
2022-02-27 14:17:42 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-function App() {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from "./Header";
+import LandingPage from "./LandingPage";
+import Map from "./Map";
+import Portfolios from "./Portfolios";
+import Schedule from "./Schedule";
+import Account from "./Account";
+
+export default function App() {
   return (
-    <div className="App">
+    <div className="container-fluid">
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="Map" element={<Map />} />
+        <Route path="Portfolios" element={<Portfolios />} />
+        <Route path="Schedule" element={<Schedule />} />
+        <Route path="Account" element={<Account />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
---
+++ src/LandingPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div>
+        <h1>This is Landing Page</h1>
+    </div>
+    );}
