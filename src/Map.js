2c8960316d4da601867f1ba1324883275bbc81be
Helen Li <helenli@Helens-MacBook-Pro.local>
2022-02-28 21:38:39 -0800

    establish map

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const position = [47.6076, -122.3362];
 
 export default function Map() {
     return (
-        <div>
-        <h1>This is Maps Page</h1>
-    </div>
-    );}
+        <MapContainer center={position} zoom={9}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    TT661 Tattoo <br /> 11:00 AM - 3:00 PM
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
